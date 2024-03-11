25ced5f2b78c33844946e44e7cca1720ea0d4778
bastien prompsy <bastienprompsy@gmail.com>
2024-03-11 15:00:55 +0100

    change fonction add

--- src/modules/static/static.service.ts
+++ src/modules/static/static.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class StaticService {
-  addNumbers(a: number, b: number): number {
-    return a + b;
+  addNumbers(a: any, b: any): number {
+    const numA = parseFloat(a);
+    const numB = parseFloat(b);
+
+    if (!isNaN(numA) && !isNaN(numB)) {
+      return numA + numB;
+    } else {
+      return NaN;
+    }
   }
 }
